7e06ca3f97ba5cef2af9b35a293503b339f0179b
Brian Nguyen <vanpho02@gmail.com>
2023-08-27 20:43:39 +0700

    feat: e2e
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { HealthCheckResolver } from './healthcheck.resolver';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
     }),
     RecipeModule,
     UserModule,
