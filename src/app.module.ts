af702badfed199209836d626a557ac6fe0b262d0
Brian Nguyen <vanpho02@gmail.com>
2023-10-21 13:32:04 +0700

    feat: minimal
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { DryerModule } from './dryerjs';
 import { Author } from './models/author';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
@@ -20,13 +14,6 @@ import { Author } from './models/author';
       installSubscriptionHandlers: true,
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.get('MONGO_URL') ?? '',
-      }),
     }),
     DryerModule.register({
       definitions: [Author],
